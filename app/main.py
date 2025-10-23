3d324d9a5828ee5adb9f5b15d416b8cdba0d9618
Alejandro Rello <alejandrorello.dev@gmail.com>
2025-10-23 13:00:04 +0200

    REST API funcionando

--- app/main.py
+++ app/main.py
@@ -1,37 +1,20 @@
 from fastapi import FastAPI
-from dotenv import load_dotenv
-import os
 
 
-# Variables de entorno
-load_dotenv()
+# Crear app FastAPI 
+app =FastAPI()
 
-# Aplicación FastAPI
-app = FastAPI(
-    title=os.getenv("APP_NAME", "Mi REST API"),
-    description="Rest API simple con FASTAPI",
-    version="1.0.0"
-)
-
-# Ruta de prueba
-@app.get("/info")
-async def read_root():
-    return {"message": "¡Bienvenido!"}
+# Ruta principal
+@app.get("/")
+def read_root():
+    return {"message": "Funciona!!"}
 
 # Ruta de salud
 @app.get("/health")
-async def health_check():
-    return {"status": "healthy", "version": "1.0.0"}
+def health_check():
+    return {"status": "healthy"}
 
-# Ruta de info
+# Ruta de información
 @app.get("/info")
-async def get_info():
-    return {
-        "app_name": "Mi REST API",
-        "version": "1.0.0",
-        "endpoints": ["/", "/health", "/info", "/docs"]
-    } 
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+def get_info():
+    return {"name": "Mi API", "verson": "1.0.0"}
