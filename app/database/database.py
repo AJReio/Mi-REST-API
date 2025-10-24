c6dbadaec95968fa51319e9f8517e9d66be54b1b
Alejandro Rello <alejandrorello.dev@gmail.com>
2025-10-24 12:51:37 +0200

    Iniciado proceso de creación de base de datos, añadidas nuevas dependencias y modelos de usuario y post.

---
+++ app/database/database.py
@@ -0,0 +1,28 @@
+from sqlalchemy import create_engine
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+
+# URL base de datos (SQLite)
+SQLALCHEMY_DATABASE_URL = "sqlite:///./app.db"
+
+
+# Motor base de datos
+engine = create_engine(
+    SQLALCHEMY_DATABASE_URL,
+    connect_args={"check_same_thread": False} # Solo para SQLite
+)
+
+# Sesión base de datos
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Base Modelos
+Base = declarative_base()
+
+# Dependencia para obtener sesión DB
+def get_db():
+    db = SessionLocal()
+    try:
+        yield db
+    finally:
+        db.close()
---
+++ app/models/models.py
@@ -0,0 +1,33 @@
+from sqlalchemy import Column, Integer, String, DateTime, Boolean, Text
+from sqlalchemy import func
+from app.database.database import Base
+
+
+# Modelo de usuario
+class User(Base):
+    __tablename__="users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    username = Column(String, unique=True, index=True, nullable=False)
+    hashed_password = Column(String, nullable=False)
+    full_name = Column(String)
+    is_active = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+
+    def __repr__(self):
+        return f"<User(id={self.id}, email={self.email}, username={self.username})>"
+    
+    # Modelo de post
+    __tablename__ = "posts"
+
+    id = Column(Integer, primary_key=True, index=True)
+    title = Column(String, index=True, nullable=True)
+    content = Column(Text, Nullable=False)
+    author_id = Column(Integer, nullable=False)
+    is_published = Column(Boolean, default=True)
+    created_at = Column(DateTime(timezone=True), server_default=func.now())
+    updated_at = Column(DateTime(timezone=True), onupdate=func.now())
+
+    def __repr__ (self):
+        return f"<Post(id={self.id}, title={self.title}, author_id={self.author_id})>"
